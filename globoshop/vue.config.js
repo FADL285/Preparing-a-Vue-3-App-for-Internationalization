4a2f6380487ecdd808b1a8c574b6d49803fa9bfa
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-09-18 01:14:01 +0200

    Reduce bundle size with feature build flags
    https://vue-i18n.intlify.dev/guide/advanced/optimization.html#reduce-bundle-size-with-feature-build-flags

--- globoshop/vue.config.js
+++ globoshop/vue.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-  chainWebpack: config => {
-    config.plugin("html").tap(args => {
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
       args[0].title = "GloboShop";
       return args;
     });
-  }
+    config.plugin("define").tap((args) => {
+      args[0] = {
+        ...args[0],
+        __VUE_I18N_FULL_INSTALL__: true,
+        __VUE_I18N_LEGACY_API__: false,
+        __INTLIFY_PROD_DEVTOOLS__: false,
+      };
+      return args;
+    });
+  },
 };
